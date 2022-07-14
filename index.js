fcbd5f8576e00ebc9fe883327eae60f58bf7ddae
rbarghou <ramsey.barghouti@superpedestrian.com>
2022-07-14 14:47:32 -0400

    added create customer stuff

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
-// Step two: create a dropin instance using that container (or a string
-//   that functions as a query selector such as `#dropin-container`)
+createCustomer = function(){
+    fetch("http://localhost:5000/create_customer", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "first_name": "Jen",
+            "last_name": "Smith",
+            "company": "Braintree",
+            "email": "jen@example.com",
+            "phone": "312.555.1234",
+            "fax": "614.555.5678",
+            "website": "www.example.com"
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("customerId").innerText = data.customer_id;
+        })
+        .catch(console.log)
+}
 
-fetch("http://localhost:5000/client_token")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.token);
-        let CLIENT_TOKEN_FROM_SERVER = data.token;
 
-        braintree.dropin.create({
-            container: document.getElementById('dropin-container'),
-            authorization: CLIENT_TOKEN_FROM_SERVER,
-            container: '#dropin-container'
-            // ...plus remaining configuration
-        }, (error, dropinInstance) => {
-            if (error) console.log(error);
-            dropinInstance.requestPaymentMethod((error, payload) => {
-                console.log(payload);
-                if (error) console.error(error);
+loadPaypalInterface = function(){
+    fetch("http://localhost:5000/client_token")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.token);
+            let CLIENT_TOKEN_FROM_SERVER = data.token;
 
-                // Step four: when the user is ready to complete their
-                //   transaction, use the dropinInstance to get a payment
-                //   method nonce for the user's selected payment method, then add
-                //   it a the hidden field before submitting the complete form to
-                //   a server-side integration
-                document.getElementById('nonce').value = payload.nonce;
-                form.submit();
+            braintree.dropin.create({
+                container: document.getElementById('dropin-container'),
+                authorization: CLIENT_TOKEN_FROM_SERVER,
+            }, (error, dropinInstance) => {
+                if (error) console.log(error);
+                dropinInstance.requestPaymentMethod((error, payload) => {
+                    console.log(payload);
+                    if (error) console.error(error);
+
+                    // Step four: when the user is ready to complete their
+                    //   transaction, use the dropinInstance to get a payment
+                    //   method nonce for the user's selected payment method, then add
+                    //   it a the hidden field before submitting the complete form to
+                    //   a server-side integration
+                    document.getElementById('nonce').value = payload.nonce;
+                    form.submit();
+                });
             });
         });
-    });
-
-
+}
